530efeef96b45a4aa715355a8d5a0df853036b59
Ray Ming (rming) <rming@micron.com>
2021-02-10 18:12:55 +0800

    Update Script

--- Bill Email Automation/SendEmail.py
+++ Bill Email Automation/SendEmail.py
@@ -4,6 +4,42 @@ import os
 from pandas import DataFrame, Series
 from datetime import datetime
 
+def mailSend(df):
+    outlook = win32.Dispatch('Outlook.application')
+    mail = outlook.CreateItem(0)
+
+    if singtelACCNumDict[group][1] == "IMFS":
+        mail.To = "munwei@micron.com;michaelwong@micron.com;"
+        mail.CC = "f10itmobile@micron.com;rming@micron.com"
+
+    if singtelACCNumDict[group][1] == "F10":
+        
+        mail.To = "danielkoh@micron.com;zhoutianchi@micron.com"
+        mail.CC = "siewkian@micron.com;rming@micron.com;f10itmobile@micron.com;"
+
+    if singtelACCNumDict[group][1] == "MSB":
+        mail.To = "rming@micron.com"
+        #mail.To = "tanlihock@micron.com;suhaimi@micron.com"
+
+
+
+    mail.Display()
+    mail.Subject = f'Mobile Payroll Deduction for Billing {date.strftime("%b")} {date.year} Mobile Usage Excess (Singtel Account number {singtelACCNumDict[group][0]})'
+    mail.HTMLbody =f'''
+    <p style="font-size: 18px;>Hi,</p>
+    <br>
+    <p style="font-size: 18px;>Below table for {group} {date.strftime("%b")} {date.year} mobile usage excess deduction. Please review.</p>
+    <body>
+    <h3>{group} -{singtelACCNumDict[group][0]}</h3>
+    {df.to_html()}
+    </body>
+    <p style="font-size: 18px;><br></p>
+    <p style="font-size: 18px;>Thank you</p>
+    '''
+
+    mail.Send()
+    
+    
 print('Running SendEmail Code')
 
 singtelACCNumDict = {
@@ -50,35 +86,20 @@ try:
 except:
     date = datetime.now()
 
-outlook = win32.Dispatch('Outlook.application')
-mail = outlook.CreateItem(0)
+print(billdf)
 
-if singtelACCNumDict[group][1] == "IMFS":
-    mail.To = "munwei@micron.com;michaelwong@micron.com;"
-    mail.CC = "f10itmobile@micron.com;rming@micron.com"
 
-if singtelACCNumDict[group][1] == "F10":
+waiting = True
+while waiting:
+    userinput = str(input('Send Email y/n ? ').lower())
+    if userinput == 'y':
+        mailSend(billdf)
+        break
     
-    mail.To = "danielkoh@micron.com;zhoutianchi@micron.com"
-    mail.CC = "siewkian@micron.com;rming@micron.com;f10itmobile@micron.com;"
+    elif userinput == 'n':
+        break
+    
+    else:
+        print("Please input only y or n")
+    
 
-if singtelACCNumDict[group][1] == "MSB":
-    mail.To = "tanlihock@micron.com;suhaimi@micron.com"
-
-
-
-mail.Display()
-mail.Subject = f'Mobile Payroll Deduction for Billing {date.strftime("%b")} {date.year} Mobile Usage Excess (Singtel Account number {singtelACCNumDict[group][0]})'
-mail.HTMLbody =f'''
-<p style="font-size: 18px;>Hi,</p>
-<br>
-<p style="font-size: 18px;>Below table for {group} {date.strftime("%b")} {date.year} mobile usage excess deduction. Please review.</p>
-<body>
-<h3>{group} -{singtelACCNumDict[group][0]}</h3>
-{billdf.to_html()}
-</body>
-<p style="font-size: 18px;><br></p>
-<p style="font-size: 18px;>Thank you</p>
-'''
-
-mail.Send()
